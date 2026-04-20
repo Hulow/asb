f8ed4e5e9fe5df5f5f3c2dc7ba6b97534d92e004
viq hulow <viq.hlw@gmail.com>
2026-04-20 16:40:34 +0200

    chore: remove purge queue

--- index.ts
+++ index.ts
@@ -8,7 +8,6 @@ async function main() {
 
     const connection = new Channel(config);
     await connection.init();
-    await connection.purgeQueue();
     await connection.addExchange();
     await connection.addQueue();
     await connection.bindQueue();
